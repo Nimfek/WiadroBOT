4b26394cba6e5a4083be48b8acf37adcb1f1c615
Nimfek <44689605+Nimfek@users.noreply.github.com>
2018-11-02 11:27:22 +0100

    Update morda.js

--- commands/morda.js
+++ commands/morda.js
@@ -1,43 +1,48 @@
 const Discord = require("discord.js");
+const ms = require("ms");
 
 module.exports.run = async (bot, message, args) => {
-    
-    let tomute = message.guild.member(message.mentions.users.first() || messsage.guild.members.get(args[0]));
-    if(!tomute) return message.reply("Byku, nikt z osiedla nie zna tego typa. Użyj komendy $komendy");
-    if(tomute.hasPermission("ADMINISTRATOR")) return message.reply("Ten kolo nie moze zamknąć mordy.");
-    let muterole = message.guild.roles.find(`name`, "muted");
-    if(!muterole)
-        {
-            try{
-                muterole = await message.guild.createRole({
-                    name: "muted",
-                    color: "#000000",
-                    permissions: []
-                })
-                message.guild.channels.forEach(async (channel, id) => {
-                    await channel.overwritePermissions(muterole, {
-                        SEND_MESSAGES: false,
-                        ADD_REACTIONS: false
-                    });
-                });
-            }catch(e){
-                console.log(e.stack);
-            }
-        }
-    let mutetime = args[1];
-    if(!mutetime) return message.reply("Te, a na ile ma zamknąć morde?")
-    
-    
-    await(tomute.addRole(muterole.id));
-    message.reply(`<@${tomute.id}> zamknął morde na ${ms(ms(mutetime))}`);
-    
-    setTimeout(function(){
-        tomute.removeRole(muterole.id);
-        message.channel.send(`<@${tomute.id}> może już otworzyć morde.`);
-    }, ms(mutetime));
-    
+
+  //!tempmute @user 1s/m/h/d
+
+  let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!tomute) return message.reply("Byku, nikt z osiedla nie zna tego typa. Użyj komendy $komendy");
+  if(tomute.hasPermission("MANAGE_MESSAGES")) return message.reply("Can't mute them!");
+  let muterole = message.guild.roles.find(`name`, "utkany");
+  //start of create role
+  if(!muterole){
+    try{
+      muterole = await message.guild.createRole({
+        name: "utkany",
+        color: "#000000",
+        permissions:[]
+      })
+      message.guild.channels.forEach(async (channel, id) => {
+        await channel.overwritePermissions(muterole, {
+          SEND_MESSAGES: false,
+          ADD_REACTIONS: false
+        });
+      });
+    }catch(e){
+      console.log(e.stack);
+    }
+  }
+  //end of create role
+  let mutetime = args[1];
+  if(!mutetime) return message.reply("Te, a na ile mu tego knebla włożyć?");
+
+  await(tomute.addRole(muterole.id));
+  message.reply(`<@${tomute.id}> zamknął morde na ${ms(ms(mutetime))}`);
+
+  setTimeout(function(){
+    tomute.removeRole(muterole.id);
+    message.channel.send(`<@${tomute.id}> może już otworzyć morde!`);
+  }, ms(mutetime));
+
+
+//end of module
 }
 
 module.exports.help = {
-    name: "morda"
+  name: "morda"
 }
